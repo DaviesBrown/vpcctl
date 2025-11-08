ac5d0dd8ca8685359f057a00aad6ffa3acd56e68
Nwosu David <nwosudavid77@gmail.com>
2025-11-08 09:38:36 +0100

    added function to delete bridge

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -41,8 +41,18 @@ class NetworkUtils:
         Create a linux bridge - (Router Implementation)
         """
         self.logger.info(f"Creating bridge: {bridge_name}")
-        self.run_command(f"ip link add {bridge_name} up")
+        self.run_command(f"ip link add {bridge_name} type bridge")
+        self.run_command(f"ip link set {bridge_name} up")
         self.logger.info(f"Bridge {bridge_name} create and activated")
+
+    def delete_bridge(self, bridge_name):
+        """
+        Deletes a linux bridge
+        """
+        self.logger.info(f"Deleting bridge: {bridge_name}")
+        self.run_command(f"ip link set {bridge_name} down", check=False)
+        self.run_command(f"ip link delete {bridge_name}", check=False)
+        self.logger.info(f"Bridge {bridge_name} deleted successfully")
 
     def create_network(self):
         """"""
