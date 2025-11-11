58b9d25ccd4ac14e8dc2f02d2bb9041b571e0f67
Nwosu David <nwosudavid77@gmail.com>
2025-11-11 11:21:22 +0100

    feat: add networking utilities for interface management and NAT setup

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -13,10 +13,10 @@ class NetworkUtils:
     """
     Handles all the direct linux networking commands
     """
-    
+
     def __init__(self):
         self.logger = logging.getLogger('vpcctl')
-    
+
     def run_command(self, command, check=True):
         """
         Run a shell command and handle errors
@@ -87,3 +87,117 @@ class NetworkUtils:
         self.run_command(f"ip link set {veth1} up")
         self.run_command(f"ip link set {veth2} up")
         self.logger.info(f"Created veth pair: {veth1}, {veth2}")
+
+    def attach_to_bridge(self, bridge_name, interface):
+        """
+        Attach an interface to a bridge
+        """
+        self.logger.info(f"Attaching {interface} to bridge {bridge_name}")
+        self.run_command(f"ip link set {interface} master {bridge_name}")
+
+    def move_to_namespace(self, interface, namespace):
+        """
+        Move an interface to a network namespace
+        """
+        self.logger.info(f"Moving {interface} to namespace {namespace}")
+        self.run_command(f"ip link set {interface} netns {namespace}")
+
+    def set_ip_address(self, namespace, interface, ip_address):
+        """
+        Set IP address on interface in namespace
+        """
+        self.logger.info(
+            f"Setting IP {ip_address} on {interface} in {namespace}")
+        self.run_in_namespace(
+            namespace, f"ip addr add {ip_address} dev {interface}")
+        self.run_in_namespace(namespace, f"ip link set {interface} up")
+
+    def set_bridge_ip(self, bridge_name, ip_address):
+        """
+        Set IP address on bridge
+        """
+        self.logger.info(f"Setting IP {ip_address} on bridge {bridge_name}")
+        self.run_command(f"ip addr add {ip_address} dev {bridge_name}")
+
+    def add_default_route(self, namespace, gateway_ip):
+        """
+        Add default route in namespace
+        """
+        self.logger.info(
+            f"Adding default route via {gateway_ip} in {namespace}")
+        self.run_in_namespace(
+            namespace, f"ip route add default via {gateway_ip}")
+
+    def enable_ip_forwarding(self):
+        """
+        Enable IP forwarding on host
+        """
+        self.logger.info("Enabling IP forwarding")
+        self.run_command("sysctl -w net.ipv4.ip_forward=1")
+
+    def setup_nat(self, bridge_name, internet_interface):
+        """
+        Setup NAT for outbound traffic from bridge
+        """
+        self.logger.info(
+            f"Setting up NAT from {bridge_name} to {internet_interface}")
+        self.enable_ip_forwarding()
+        self.run_command(
+            f"iptables -t nat -A POSTROUTING -o {internet_interface} -j MASQUERADE"
+        )
+        self.run_command(
+            f"iptables -A FORWARD -i {bridge_name} -o {internet_interface} -j ACCEPT"
+        )
+        self.run_command(
+            f"iptables -A FORWARD -i {internet_interface} -o {bridge_name} -m state --state RELATED,ESTABLISHED -j ACCEPT"
+        )
+
+    def add_route(self, namespace, destination, gateway):
+        """
+        Add a route in namespace
+        """
+        self.logger.info(
+            f"Adding route to {destination} via {gateway} in {namespace}")
+        self.run_in_namespace(
+            namespace, f"ip route add {destination} via {gateway}")
+
+    def apply_firewall_rule(self, namespace, rule):
+        """
+        Apply iptables firewall rule in namespace
+        """
+        protocol = rule.get('protocol', 'tcp')
+        port = rule.get('port')
+        action = rule.get('action', 'allow').upper()
+
+        if action == 'ALLOW':
+            target = 'ACCEPT'
+        elif action == 'DENY':
+            target = 'DROP'
+        else:
+            target = action
+
+        if port:
+            rule_cmd = f"iptables -A INPUT -p {protocol} --dport {port} -j {target}"
+        else:
+            rule_cmd = f"iptables -A INPUT -p {protocol} -j {target}"
+
+        self.logger.info(f"Applying firewall rule in {namespace}: {rule_cmd}")
+        self.run_in_namespace(namespace, rule_cmd)
+
+    def cleanup_nat_rules(self, bridge_name, internet_interface):
+        """
+        Cleanup NAT rules
+        """
+        self.logger.info(f"Cleaning up NAT rules for {bridge_name}")
+        self.run_command(
+            f"iptables -t nat -D POSTROUTING -o {internet_interface} -j MASQUERADE",
+            check=False
+        )
+        self.run_command(
+            f"iptables -D FORWARD -i {bridge_name} -o {internet_interface} -j ACCEPT",
+            check=False
+        )
+        self.run_command(
+            f"iptables -D FORWARD -i {internet_interface} -o {bridge_name} -m state --state RELATED,ESTABLISHED -j ACCEPT",
+            check=False
+        )
